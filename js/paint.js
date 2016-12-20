0de608310009335b07f8e0efc5cd9646f061df51
DavNej <david.nejar@gmail.com>
2016-12-21 00:15:29 +0200

    update

--- js/paint.js
+++ js/paint.js
@@ -9,26 +9,69 @@ var colors = [
     "#793ed8",
     "#d83ed3"];
 
-function createCanvas(){
+var selectedColor = 0;
+
+function initCanvas(){
     var canvas = document.createElement('div');
     canvas.id = "canvas";
     canvas.style.backgroundColor = "white";
     // canvas.style.height = "50%";
     // canvas.style.width = "50%";
+    canvas.addEventListener("click", paintItUp)
     document.body.appendChild(canvas);
 }
 
-function createColor() {
+function initColor() {
     for (var i = 0; i < colors.length; i++){
         var newColor = document.createElement('div');
         newColor.classList.add('color');
+        newColor.id = "color-" + (i+1);
         newColor.style.backgroundColor = colors[i];
-        document.getElementById('menu').appendChild(newColor);
+        newColor.addEventListener("click", selectColor);
+        document.getElementById('palette').appendChild(newColor);
     }
 }
 
+function selectColor(e) {
+    selectedColor = e.target.id;
+    console.log(selectedColor);
+}
+
+//Brush features
+function initBrushSize(){
+    var brushSize = document.getElementsByClassName('brush')[1];
+    brushSize.addEventListener("input", changeSize);
+}
+function changeSize(){
+    var brush = document.getElementsByClassName('brush');
+    brush[0].textContent = "Brush size\n" + brush[1].value + " px";
+}
+function getBrushSize(){
+    return document.getElementsByClassName('brush')[1].value + "px";
+}
+
+function paintItUp(e){
+    if (selectedColor === 0){
+        alert("Please select a color first");
+        return;
+    }
+    console.log(e.pageX);
+    var painted = document.createElement('div');
+    painted.style.backgroundColor = document.getElementById(selectedColor).style.backgroundColor;
+    painted.style.display = "inline-block";
+    painted.style.display = "absolute";
+
+    painted.style.width = getBrushSize();
+    painted.style.height = getBrushSize();
+
+
+    // painted.style.top = 
+
+    canvas.appendChild(painted);
+}
 
 function init() {
-    createCanvas();
-    createColor();
+    initCanvas();
+    initColor();
+    initBrushSize()
 }
